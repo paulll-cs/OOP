b56425e16501447524a1b85807157cbfb0956ccb
Viet Phuc Luong <a1961200@emb108w034.ad.adelaide.edu.au>
2025-08-01 16:18:24 +0930

    Prac 1

---
+++ Easy problems/1-3.cpp
@@ -0,0 +1,13 @@
+int num_count(int array[], int n, int number){
+    int count = 0;
+    int i;
+    if (n<1){
+        return 0;
+    }else {
+            for (i=0;i<n;i++){
+            if (array[i] == number){
+                count++;
+            }
+        }   
+    }
+}
