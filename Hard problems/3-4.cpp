27bce304cc4867f09e5b5f02ce33d3125e73ae92
unknown <a1961200@adelaide.edu.au>
2025-08-02 17:23:57 +0930

    All questions answered

--- Hard problems/3-4.cpp
+++ Hard problems/3-4.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+
 void print_pass_fail(char grade){
-    
+    switch(grade){
+                case 'A': 
+                    std::cout<< "Pass\n";
+                    break;
+                case 'B':
+                    std::cout<< "Pass\n";
+                    break;
+                case 'C':
+                    std::cout<< "Pass\n";
+                    break;
+                case 'D':
+                    std::cout<< "Fail\n";
+                    break;
+                case 'E':
+                    std::cout<< "Fail\n";
+                    break;
+                default:
+                    std::cout<< "Nothing\n";
+                    break;
+            }
 }
