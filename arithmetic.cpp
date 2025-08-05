b1448b081ab47ec6d6595a45108c348d6c17ea14
unknown <a1961200@adelaide.edu.au>
2025-08-05 17:10:17 +0930

    hola

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,10 +1,10 @@
 #include <string>
 #include <iostream>
 
-std::string binary_addition(int base_2_in_1,int base_2_in_2,std::string base_2_out ="",int carry = 0){
+std::string binary_addition(int base_2_in_1,int base_2_in_2,std::string base_2_out ="0",int carry = 0){
     std::string remainder = "";
+    int total;
     int result;
-    int carry;
 
     if (base_2_in_1/10 == 0|| base_2_in_2/10 == 0){
         return "1" + base_2_out;
@@ -13,14 +13,14 @@ std::string binary_addition(int base_2_in_1,int base_2_in_2,std::string base_2_o
     int last_digit_1 = base_2_in_1%10;
     int last_digit_2 = base_2_in_2%10;
 
-    result = last_digit_1+last_digit_2+carry;
-    remainder = std::to_string(result);
-    if (carry != 0){
-        carry = result-1;
+    total = last_digit_1+last_digit_1;
+    carry -= 1-total;
+    if (total >= 1){
+        remainder = "1";
     }else{
-        carry = 0;
+        remainder = "0";
     }
-
+    remainder = std::to_string(result);
     int last_digit_removed_1 = (base_2_in_1 - last_digit_1)/10;
     int last_digit_removed_2 = (base_2_in_2 - last_digit_2)/10;
 
