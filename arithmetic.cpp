cfaa049d5b4858a6fb56bc91eee5d2476aa6bde8
unknown <a1961200@adelaide.edu.au>
2025-08-05 16:48:23 +0930

    hella

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,8 +1,10 @@
 #include <string>
 #include <iostream>
 
-std::string binary_addition(int base_2_in_1,int base_2_in_2,std::string base_2_out =""){
+std::string binary_addition(int base_2_in_1,int base_2_in_2,std::string base_2_out ="",int carry = 0){
     std::string remainder = "";
+    int result;
+    int carry;
 
     if (base_2_in_1/10 == 0|| base_2_in_2/10 == 0){
         return "1" + base_2_out;
@@ -11,22 +13,23 @@ std::string binary_addition(int base_2_in_1,int base_2_in_2,std::string base_2_o
     int last_digit_1 = base_2_in_1%10;
     int last_digit_2 = base_2_in_2%10;
 
-    if (last_digit_1 ==0&& last_digit_2 ==0){
-        remainder = "0";
+    result = last_digit_1+last_digit_2+carry;
+    remainder = std::to_string(result);
+    if (carry != 0){
+        carry = result-1;
     }else{
-        int int_remainder = 1/(last_digit_1+last_digit_2);
-        remainder = std::to_string(int_remainder);
+        carry = 0;
     }
 
-    int last_digit_removed_1 = (base_2_in_1 - base_2_in_1%10)/10;
-    int last_digit_removed_2 = (base_2_in_2 - base_2_in_2%10)/10;
+    int last_digit_removed_1 = (base_2_in_1 - last_digit_1)/10;
+    int last_digit_removed_2 = (base_2_in_2 - last_digit_2)/10;
 
-    return binary_addition(last_digit_removed_1,last_digit_removed_2,remainder + base_2_out);
+    return binary_addition(last_digit_removed_1,last_digit_removed_2,remainder + base_2_out,carry);
 }
 
 int main(void)
 {
-    std::cout<<"Enter 2 binary numbers";
+    std::cout<<"Enter 2 binary numbers:\n";
     
     int binary_1=0;
     int binary_2=0;
