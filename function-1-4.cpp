f226a3db06390cf87e240f88d67933851d3c00c4
unknown <a1961200@adelaide.edu.au>
2025-08-22 16:27:31 +0930

    2

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,5 +1,19 @@
 #include "Person.h"
 
+PersonList createPersonList(int n){
+    PersonList list;
+    list.numPeople = n;
+
+    list.people = new Person[n];
+
+    for (int i=0;i<n;i++){
+        list.people[i].age = 1;
+        list.people[i].name = "Jane Doe";
+    }
+    return list;
+
+}
+
 PersonList shallowCopyPersonList(const PersonList& pl){
     PersonList copy;
     copy.numPeople = pl.numPeople;
